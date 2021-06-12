ab831d32e1a81907fb7f869444a0c5dcbfb74f36
abuhorainhero <abuhorain2016@gmail.com>
2021-06-12 21:28:11 +0600

    second

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/", indexRouter);
+app.use("/api", indexRouter);
 // ---
 app.use("/api/country", countryRouter);
 app.use("/api/city", cityRouter);
