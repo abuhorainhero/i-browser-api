3b0e94bdf500630f13c07d5acdd387bd1ad1ecca
abuhorainhero <abuhorain2016@gmail.com>
2021-06-23 22:59:56 +0600

    normal api

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // --------------- initial setup for live site ---------------
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('Frontend/build'));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('Frontend/build'));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
+//     })
+// }
 
 
 // ----------- Initial Router --------------
