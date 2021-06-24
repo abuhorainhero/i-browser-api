2870e3c5257f43e8920446c0cc167fb5d2e6cced
abuhorainhero <abuhorain2016@gmail.com>
2021-06-24 17:56:28 +0600

    url

--- app.js
+++ app.js
@@ -19,39 +19,10 @@ app.use(cookieParser());
 // --------------- initial setup for live site ---------------
 if (process.env.NODE_ENV == 'production') {
     app.use(express.static('Frontend/build'));
-    app.get(`${!"api"}*`, (req, res) => {
+    app.get(`/`, (req, res) => {
         res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
     });
-    // app.get("/home", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/ads", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/interest", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/users", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/admin", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/city", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/country", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/dashboard", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/login", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
-    // app.get("/profile", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
-    // });
+    
 }
 
 app.use(express.static(path.join(__dirname, "public")));
