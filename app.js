c2468bfb3a0a38892a0d00ca074242458bb750d2
abuhorainhero <abuhorain2016@gmail.com>
2021-06-24 14:37:18 +0600

    upd

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // --------------- initial setup for live site ---------------
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV == 'production') {
     app.use(express.static('Frontend/build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
